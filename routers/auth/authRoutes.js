abf11de0411b364b280e6c48c9ae5ef7c90c5aca
Parag Singh <98509135+parag3638@users.noreply.github.com>
2025-09-13 18:54:15 +0530

    Update authRoutes.js

--- routers/auth/authRoutes.js
+++ routers/auth/authRoutes.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const { register, login, verifyOtp, resendOtp } = require('./controllers/authcontroller.js');
+const { login, verifyOtp, resendOtp } = require('./controllers/authcontroller.js');
 const { requestReset, verifyResetOtp, completeReset } = require('./controllers/authcontroller.js');
+const { register, verifyRegisterOtp, resendRegisterOtp, } = require('../../controllers/authcontroller');
 
 const router = express.Router();
 
-router.post('/register', register);
+// Registration (2-step)
+router.post('/register', register);                 // start: send OTP
+router.post('/register/verify', verifyRegisterOtp); // finish: create user
+router.post('/register/resend-otp', resendRegisterOtp); // optional
+
+// Login (2-step)
 router.post('/login', login);
 router.post('/verify-otp', verifyOtp);
 router.post('/resend-otp', resendOtp);
@@ -13,4 +19,3 @@ router.post('/password-reset/verify', verifyResetOtp);
 router.post('/password-reset/complete', completeReset);
 
 module.exports = router;
-
